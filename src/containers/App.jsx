02cbd6f46759dcd968f0d05f8e140003cdd3e203
Renan GEHAN <rgehan94@gmail.com>
2017-08-24 00:54:04 +0200

    Move data/ out of src/

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import FuzzyFinder from '../components/FuzzyFinder.jsx';
-import randomWords from '../data/random-words';
-import reactPaths from '../data/react-paths';
+import randomWords from '../../data/random-words';
+import reactPaths from '../../data/react-paths';
 import '../styles/App.scss';
 
 class App extends Component {
